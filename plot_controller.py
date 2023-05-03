fcdd1216b3824e8e2b7a3f22951a82a51616f3d2
rstoltzm <rstoltzm@gmail.com>
2023-05-03 20:59:20 +0000

    update plots

--- plot_controller.py
+++ plot_controller.py
@@ -13,42 +13,63 @@ def register_routes(app):
 
 def interactive_plot():
     processed_data, _ = load_data()
+    date = processed_data['date']
+    buy_date = processed_data.loc[
+        processed_data['signal'] == 'BUY', 'date']
+    buy_signal = processed_data.loc[
+        processed_data['signal'] == 'BUY', 'close']
+    sell_date = processed_data.loc[
+        processed_data['signal'] == 'SELL', 'date']
+    sell_signal = processed_data.loc[
+        processed_data['signal'] == 'SELL', 'close']
+    hold_date = processed_data.loc[
+        processed_data['signal'] == 'HOLD', 'date']
+    hold_signal = processed_data.loc[
+        processed_data['signal'] == 'HOLD', 'close']
 
-    # Create a Plotly Figure
-    fig = go.Figure()
+    # Create the first Plotly Figure for close and hull_high
+    fig1 = go.Figure()
+    fig1.add_trace(go.Scatter(x=date,
+                              y=processed_data['close'],
+                              mode='lines', name='Close'))
+    fig1.add_trace(go.Scatter(x=date,
+                              y=processed_data['hull_high'],
+                              mode='lines', name='Hull High'))
+    fig1.add_trace(go.Scatter(x=buy_date,
+                              y=buy_signal,
+                              mode='markers', name='Buy',
+                              marker=dict(color='green', size=10)))
 
-    # Add the 'close' trace
-    fig.add_trace(
-        go.Scatter(x=processed_data['date'],
-                   y=processed_data['close'],
-                   mode='lines', name='Close'))
+    fig1.add_trace(go.Scatter(x=sell_date,
+                              y=sell_signal,
+                              mode='markers', name='Sell',
+                              marker=dict(color='red', size=10)))
 
-    # Add the 'tr', 'atr', and 'hull_high' traces
-    fig.add_trace(
-        go.Scatter(x=processed_data['date'],
-                   y=processed_data['tr'],
-                   mode='lines', name='TR'))
-    fig.add_trace(go.Scatter(x=processed_data['date'],
-                             y=processed_data['atr'],
-                             mode='lines', name='ATR'))
-    fig.add_trace(go.Scatter(x=processed_data['date'],
-                             y=processed_data['hull_high'],
-                             mode='lines', name='Hull High'))
+    fig1.add_trace(go.Scatter(x=hold_date,
+                              y=hold_signal,
+                              mode='markers', name='Hold',
+                              marker=dict(color='orange', size=5)))
 
-    # Add the 'signal' trace
-    fig.add_trace(go.Scatter(x=processed_data['date'],
-                             y=processed_data['signal'],
-                             mode='markers', name='Signal'))
+    fig1.update_layout(title='Close and Hull High Plot',
+                       xaxis_title='Date',
+                       yaxis_title='Value')
 
-    # Configure the layout
-    fig.update_layout(title='Processed Data Plot',
-                      xaxis_title='Date',
-                      yaxis_title='Value')
+    # Create the second Plotly Figure for tr and atr
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=processed_data['date'],
+                              y=processed_data['tr'],
+                              mode='lines', name='TR'))
+    fig2.add_trace(go.Scatter(x=processed_data['date'],
+                              y=processed_data['atr'],
+                              mode='lines', name='ATR'))
+    fig2.update_layout(title='TR and ATR Plot',
+                       xaxis_title='Date', yaxis_title='Value')
 
-    # Convert the figure to JSON
-    graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    # Convert both figures to JSON
+    graph1_json = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+    graph2_json = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return jsonify(graph_json)
+    return jsonify({'graph1': graph1_json, 'graph2': graph2_json})
 
 
 def interactive_plot_js():
