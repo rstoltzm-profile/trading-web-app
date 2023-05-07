bd29259623447a4ce051bf28cf1aa8e3a8425972
rstoltzm <rstoltzm@gmail.com>
2023-05-07 18:35:32 +0000

    updating

--- plot_controller.py
+++ plot_controller.py
@@ -11,61 +11,64 @@ def register_routes(app):
                      interactive_plot_js)
 
 
+def load_signal_data(processed_data, signal):
+    date = processed_data.loc[processed_data['signal'] == signal, 'date']
+    close = processed_data.loc[processed_data['signal'] == signal, 'close']
+    return date, close
+
+
+def create_close_hull_high_figure(processed_data, buy_date, buy_signal,
+                                  sell_date, sell_signal, hold_date,
+                                  hold_signal, wait_date, wait_signal):
+    date = processed_data['date']
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=date, y=processed_data['close'],
+                             mode='lines', name='Close'))
+    fig.add_trace(go.Scatter(x=date, y=processed_data['hull_high'],
+                             mode='lines', name='Hull High'))
+    fig.add_trace(go.Scatter(x=buy_date, y=buy_signal+10,
+                             mode='markers', name='Buy',
+                             marker=dict(color='green', size=10)))
+    fig.add_trace(go.Scatter(x=sell_date, y=sell_signal+10,
+                             mode='markers', name='Sell',
+                             marker=dict(color='red', size=10)))
+    fig.add_trace(go.Scatter(x=hold_date, y=hold_signal+10,
+                             mode='markers', name='Hold',
+                             marker=dict(color='orange', size=3)))
+    fig.add_trace(go.Scatter(x=wait_date, y=wait_signal+10,
+                             mode='markers', name='Wait',
+                             marker=dict(color='yellow', size=3)))
+    fig.update_layout(title='Close and Hull High Plot',
+                      xaxis_title='Date',
+                      yaxis_title='Value')
+    return fig
+
+
+def create_tr_atr_figure(processed_data):
+    date = processed_data['date']
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=date, y=processed_data['tr'],
+                             mode='lines', name='TR'))
+    fig.add_trace(go.Scatter(x=date, y=processed_data['atr'],
+                             mode='lines', name='ATR'))
+    fig.update_layout(title='TR and ATR Plot', xaxis_title='Date',
+                      yaxis_title='Value')
+    return fig
+
+
 def interactive_plot():
     processed_data, _ = load_data()
-    date = processed_data['date']
-    buy_date = processed_data.loc[
-        processed_data['signal'] == 'BUY', 'date']
-    buy_signal = processed_data.loc[
-        processed_data['signal'] == 'BUY', 'close']
-    sell_date = processed_data.loc[
-        processed_data['signal'] == 'SELL', 'date']
-    sell_signal = processed_data.loc[
-        processed_data['signal'] == 'SELL', 'close']
-    hold_date = processed_data.loc[
-        processed_data['signal'] == 'HOLD', 'date']
-    hold_signal = processed_data.loc[
-        processed_data['signal'] == 'HOLD', 'close']
 
-    # Create the first Plotly Figure for close and hull_high
-    fig1 = go.Figure()
-    fig1.add_trace(go.Scatter(x=date,
-                              y=processed_data['close'],
-                              mode='lines', name='Close'))
-    fig1.add_trace(go.Scatter(x=date,
-                              y=processed_data['hull_high'],
-                              mode='lines', name='Hull High'))
-    fig1.add_trace(go.Scatter(x=buy_date,
-                              y=buy_signal,
-                              mode='markers', name='Buy',
-                              marker=dict(color='green', size=10)))
+    buy_date, buy_signal = load_signal_data(processed_data, 'BUY')
+    sell_date, sell_signal = load_signal_data(processed_data, 'SELL')
+    hold_date, hold_signal = load_signal_data(processed_data, 'HOLD')
+    wait_date, wait_signal = load_signal_data(processed_data, 'WAIT')
 
-    fig1.add_trace(go.Scatter(x=sell_date,
-                              y=sell_signal,
-                              mode='markers', name='Sell',
-                              marker=dict(color='red', size=10)))
+    fig1 = create_close_hull_high_figure(processed_data, buy_date, buy_signal,
+                                         sell_date, sell_signal, hold_date,
+                                         hold_signal, wait_date, wait_signal)
+    fig2 = create_tr_atr_figure(processed_data)
 
-    fig1.add_trace(go.Scatter(x=hold_date,
-                              y=hold_signal,
-                              mode='markers', name='Hold',
-                              marker=dict(color='orange', size=5)))
-
-    fig1.update_layout(title='Close and Hull High Plot',
-                       xaxis_title='Date',
-                       yaxis_title='Value')
-
-    # Create the second Plotly Figure for tr and atr
-    fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(x=processed_data['date'],
-                              y=processed_data['tr'],
-                              mode='lines', name='TR'))
-    fig2.add_trace(go.Scatter(x=processed_data['date'],
-                              y=processed_data['atr'],
-                              mode='lines', name='ATR'))
-    fig2.update_layout(title='TR and ATR Plot',
-                       xaxis_title='Date', yaxis_title='Value')
-
-    # Convert both figures to JSON
     graph1_json = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
     graph2_json = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
 
