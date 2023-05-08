2d985a2b1ee930476d388a57ae8d4192b917d77d
rstoltzm <rstoltzm@gmail.com>
2023-05-08 22:04:55 +0000

    updated with buttons

--- blueprints/plot_controller.py
+++ blueprints/plot_controller.py
@@ -1,18 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 import json
 import plotly
 import plotly.graph_objects as go
 from blueprints.data_loader import load_data
+import pandas as pd
+from datetime import timedelta
+
 
 plot_bp = Blueprint('plot_bp', __name__)
 
 
 @plot_bp.route('/interactive_plot')
 def interactive_plot():
+    days = request.args.get('days', default=None)
     processed_data, _ = load_data()
-    fig1 = break_out_figure(processed_data)
+    if days == 'all':
+        days = None
+    filtered_data = filter_data(processed_data, days)
+    fig1 = break_out_figure(filtered_data)
     graph_json = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template("interactive_plot.html", graph_json=graph_json)
+
+
+def filter_data(processed_data, days):
+    if days is None or days == 'all':
+        return processed_data
+
+    days = int(days)
+    print(days, flush=True)
+    processed_data['date'] = pd.to_datetime(processed_data['date'])
+    end_date = pd.to_datetime(processed_data['date'].max())
+    start_date = end_date - timedelta(days=days)
+    filtered_data = processed_data.loc[processed_data['date'] >= start_date]
+    return filtered_data
 
 
 def load_signal_data(processed_data, signal):
@@ -23,15 +43,10 @@ def load_signal_data(processed_data, signal):
 
 def break_out_figure(processed_data):
     # Setup Markers
-    marker_offset = 10
     buy_date, buy_signal = load_signal_data(processed_data, 'BUY')
     sell_date, sell_signal = load_signal_data(processed_data, 'SELL')
     hold_date, hold_signal = load_signal_data(processed_data, 'HOLD')
     wait_date, wait_signal = load_signal_data(processed_data, 'WAIT')
-    sell_signal += marker_offset
-    buy_signal += marker_offset
-    wait_signal += marker_offset
-    hold_signal += marker_offset
 
     # Setup Data
     x = processed_data['date']
@@ -73,7 +88,8 @@ def break_out_figure(processed_data):
         name='Buy',
         marker=dict(
             color='green',
-            size=10)
+            size=10,
+            symbol='triangle-up')
         ))
     fig.add_trace(go.Scatter(
         x=sell_date,
@@ -82,7 +98,8 @@ def break_out_figure(processed_data):
         name='Sell',
         marker=dict(
             color='red',
-            size=10)
+            size=10,
+            symbol='triangle-down')
         ))
     fig.add_trace(go.Scatter(
         x=hold_date,
@@ -91,7 +108,12 @@ def break_out_figure(processed_data):
         name='Hold',
         marker=dict(
             color='orange',
-            size=3)
+            size=10,
+            symbol='x',
+            line=dict(
+                color='orange',
+                width=1
+            ))
         ))
     fig.add_trace(go.Scatter(
         x=wait_date,
@@ -99,8 +121,9 @@ def break_out_figure(processed_data):
         mode='markers',
         name='Wait',
         marker=dict(
-            color='yellow',
-            size=3)
+            color='blue',
+            size=8,
+            symbol='x')
         ))
     fig.update_layout(
         title='Break Out Strategy',
