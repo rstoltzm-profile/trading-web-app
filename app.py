738d810bd0b8f376765c7804175701682cabfca2
ryan_ec2 <rstoltzm@gmail.com>
2023-05-30 22:41:28 +0000

    update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from blueprints.status_controller import status_bp
 from blueprints.homepage import home_bp
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 app.register_blueprint(status_bp)
 app.register_blueprint(plot_bp)
--- blueprints/status_controller.py
+++ blueprints/status_controller.py
@@ -7,4 +7,21 @@ status_bp = Blueprint('status', __name__)
 @status_bp.route('/status')
 def status():
     _, status_data = load_data()
-    return render_template("status.html", status_data=status_data.to_html())
+    status_data = status_data[['strategy','last_signal', 'last_signal_date', 'data_status', 'last job time', 'percent_profit', 'max_drawdown', 'algo_start', 'live_start_date', 'live_start_funds']]
+    status_data = status_data.rename(
+        columns={'strategy': 'Strategy',
+                 'data_status': 'Latest Stock Data',
+                 'last_signal': 'Latest Signal',
+                 'last_signal_date': 'Latest Signal Date',
+                 'percent_profit': 'Profit %',
+                 'max_drawdown': 'Max Drawdown %',
+                 'algo_start': 'Algo Data Start',
+                 'live_start_date': 'Live Start Date',
+                 'live_start_funds': 'Live Cash',
+                 'last job time': 'Latest Job Run',
+                 }
+        )
+    status_data = status_data.iloc[0]
+    status_data = status_data.to_frame().reset_index()
+    status_data.columns = ['column_name', 'value']
+    return render_template("status.html", table=status_data)
